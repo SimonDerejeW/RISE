9cb8c5397054d154449a99427c733b392eca8eea
amha-mersha <amhamersh@gmail.com>
2024-07-17 20:44:14 +0300

    hero section modified

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -9,10 +9,14 @@ const LandingPage = () => {
       <main>
         <div>
             <div>
-
+              <div className="bg-blue-600 rounded-lg">
+                <p className="font-bold text-6xl text-white">
+                  Connecting Refugees, Building Communities    
+                </p>
+              </div>
             </div>
             <div className="rounded-lg ">
-                <img src="image" alt="" />
+                <img src={image} alt="" />
             </div>
         </div>
       </main>
