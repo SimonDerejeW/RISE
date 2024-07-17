51fef333bf7ee686184c0b321778a680a55367fe
Sophonias Demiss <sophidemiss@gmail.com>
2024-07-17 10:39:00 +0300

    some improvement on the list

--- frontend/src/components/Userlist/HeaderUser.tsx
+++ frontend/src/components/Userlist/HeaderUser.tsx
@@ -4,8 +4,8 @@ import h from '../../images/rise-high-resolution-logo-transparent 1.png'
 const HeaderUser = () => {
   return (
     <>
-        <header className='bg-white h-16'>
-            <img className='p-3' src={h} alt="" />
+        <header className='bg-white h-12'>
+            <img height={80} width={100} className='p-3' src={h} alt="" />
         </header>
     </>
   )
